668b6e761c0e1f4bc1c279ba8da792578b5278ca
Joshua Dupin <joshua35260@gmail.com>
2023-02-17 11:16:23 +0100

    finit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ROUTES } from './app.routes';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from '../components/menu/menu.component';
+import { SignUpComponent } from '../components/sign-up/sign-up.component';
+import { UserProfileComponent } from '../components/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent,
+    SignUpComponent,
+    MenuComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
