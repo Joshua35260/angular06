def30e4f2e2906c6e41bbc33a2fb4f4066f6ac2f
Joshua Dupin <joshua35260@gmail.com>
2023-02-21 16:01:50 +0100

    atelier done, quest 06 in progress

--- src/components/login-form/login-form.component.ts
+++ src/components/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
+  loginForm: FormGroup;
+  formSubmitted = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.formSubmitted = true;
+    if (this.loginForm.valid) {
+      console.log('Login validé');
+    } else {
+      console.log('Le formulaire est invalide');
+    }
+  }
 }
