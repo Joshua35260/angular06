302eec9650d5a4f46c63b5bf3de5d9debcd3ba5e
Joshua Dupin <joshua35260@gmail.com>
2023-02-22 05:36:43 +0100

    dodo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 
-import { UserProfileComponent } from '../components/user-profile/user-profile.component';
 import { SignUpComponent } from '../components/sign-up/sign-up.component';
-
 import { LoginFormComponent } from '../components/login-form/login-form.component';
 import { MovieSearchComponent } from '../components/search-movie/search-movie.component';
 
 const ROUTES: Routes =[
-  {path: '', component: UserProfileComponent},
+  {path: '', component: MovieSearchComponent},
   {path: 'sign-up', component: SignUpComponent},
   {path: 'movie', component: MovieSearchComponent},
   {path: 'login', component: LoginFormComponent},
